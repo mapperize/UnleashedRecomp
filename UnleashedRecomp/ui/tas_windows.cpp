730115b81dc31283d80410202eb93551a916075a
Ethan Robards <yourtooslow@gmail.com>
2026-06-02 12:49:27 -0500

    add document-relative IGT offset, reformat timer string

--- UnleashedRecomp/ui/tas_windows.cpp
+++ UnleashedRecomp/ui/tas_windows.cpp
@@ -494,9 +494,6 @@ void TASWindow::ShowValues(uintptr_t ptr, bool isWerehog){
             
             
             be<float> timer = *(be<float>*)g_memory.Translate( &(pGameDocument->pMember) + 0x5C);
-            int minutes = 0;
-            int seconds = 0;
-            int milliseconds = 0;
             if (timer >= 0) {
 
                 int time = (int)(timer * 100);
